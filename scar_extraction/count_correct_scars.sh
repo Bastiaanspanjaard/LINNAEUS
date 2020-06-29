8a7a6fc291ff11e5113e8c29752b7fdd3adebe5b
Bastiaan Spanjaard <b.spanjaard@hubrecht.eu>
2020-06-29 11:19:59 +0200

    Added lineage tracing reference and bulk barcodes.

--- scar_extraction/count_correct_scars.sh
+++
@@ -1,3 +0,0 @@
-join -1 2 -2 2 <(sort -k2,2 bulkbc.csv) <(sort -k2,2 Bulk_DNA_1_scars.txt) > Bulk_DNA_1_scars_bbc.txt
-sort Bulk_DNA_1_scars_bbc.txt | uniq -c > Bulk_DNA_1_scar_bccounts.txt
-rm Bulk_DNA_1_scars_bbc.txt
